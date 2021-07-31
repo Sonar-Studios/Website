a0d7c83ee0b97020904303c00eb8d7d81fa788da
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-07-31 00:12:31 -0700

    update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
 import "../stylesheets/styles.css";
+import LinkButton from "../components/linkButton";
 
+import sonaria from "../images/sonaria.png";
+
+// we use a functional component for initial render because we can't use
+// hooks in class components, it sucks but that's what we gotta do
 export default () => {
 	useEffect(() => {
 		document.title = "Sonar Studios";
 	}, []);
 
 	return (
-		<div className="page">
-			<h1 className="header">
-				Hello, world!
-			</h1>
+		<div>
+			<div className="header">
+				<LinkButton text={"home"}/>
+				<LinkButton text={"games"}/>
+				<LinkButton text={"contact"}/>
+				<LinkButton text={"careers"}/>
+			</div>
+			<img className="banner" src={sonaria}>
+
+			</img>
 		</div>
 	);
 };
