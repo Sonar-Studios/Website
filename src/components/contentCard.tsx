34aaa7aa5716a7fd1523a7d7343f81357495f8ae
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-08-02 00:16:27 -0700

    add text

--- src/components/contentCard.tsx
+++ src/components/contentCard.tsx
@@ -6,6 +6,7 @@ interface ContentCardState {}
 interface ContentCardProps {
 	img: string;
 	title: string;
+	body: string;
 }
 
 export default class ContentCard extends Component<ContentCardProps, ContentCardState> {
@@ -14,6 +15,7 @@ export default class ContentCard extends Component<ContentCardProps, ContentCard
 			<div className="card">
 				<img className="card-image" src={this.props.img} />
 				<h2 className="card-title">{this.props.title.toUpperCase()}</h2>
+				<div className="card-body">{this.props.body.toUpperCase()}</div>
 			</div>
 		)
 	}
