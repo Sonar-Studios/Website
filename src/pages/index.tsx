8fcee2fe607071e4f18ea69cca172512dbc5d234
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-08-07 17:47:49 -0700

    add new section + new banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import ContentCard from "../components/contentCard";
 import PageHeader from "../components/pageHeader";
 
 import logo from "../images/logo.png";
-import sonaria from "../images/sonaria.png";
+import sitebanner from "../images/sitebanner.png";
 import dragonadventures_title from "../images/dragonadventures_title.png";
 import sonaria_title from "../images/sonaria_title.png";
 import captive_title from "../images/captive_title.png";
@@ -15,6 +15,8 @@ import warships_title from "../images/warships_title.png";
 import sunnyshores_title from "../images/sunnyshores_title.png";
 import felinesdestiny_title from "../images/felinesdestiny_title.png";
 import StatCard from "../components/statCard";
+import PageBody from "../components/pageBody";
+import PageImage from "../components/pageImage";
 
 // we use a functional component for initial render because we can't use
 // hooks in class components, it sucks but that's what we gotta do
@@ -37,7 +39,7 @@ export default () => {
 
 			{/* homepage banner */}
 			<div className="banner">
-				<img className="banner-image" src={sonaria} />
+				<img className="banner-image" src={sitebanner} />
 				<div className="banner-reel">
 					<iframe
 						width="800"
@@ -68,63 +70,90 @@ export default () => {
 			</div>
 
 			<div className="section">
-				<PageHeader text={"titles"} />
 				<div className="content">
-					<div className="grid">
-						<ContentCard
-							img={dragonadventures_title}
-							title={"dragon adventures"}
-							link={"https://www.roblox.com/games/3475397644"}
-							body={
-								"Hatch eggs and raise your dragons to become powerful beasts in a variety of immersive worlds"
-							}
-						/>
-						<ContentCard
-							img={sonaria_title}
-							title={"creatures of sonaria"}
-							link={"https://www.roblox.com/games/5233782396"}
-							body={
-								"Grow, fight and survive with other players as a fictional creature in the beautiful world of Sonaria"
-							}
-						/>
-						<ContentCard
-							img={captive_title}
-							title={"captive"}
-							link={"https://www.roblox.com/games/3388553819"}
-							body={
-								"Hack the computers, escape the beast, unlock the exits, and escape captivity"
-							}
-						/>
-						<ContentCard
-							img={warships_title}
-							title={"warships"}
-							link={"https://www.roblox.com/games/334218733"}
-							body={
-								"Set sail in massive battleships for control over the seas"
-							}
-						/>
-						<ContentCard
-							img={sunnyshores_title}
-							title={"sunny shores"}
-							link={"https://www.roblox.com/games/7071201807"}
-							body={
-								"Build your dream home, dress up, party with friends and more in our tropical island city"
-							}
-						/>
-						<ContentCard
-							img={felinesdestiny_title}
-							title={"feline's destiny"}
-							link={"https://www.roblox.com/games/2586423667"}
-							body={
-								"Explore a rainforest, survive disasters and roleplay with friends as a wild cat"
-							}
-						/>
-					</div>
+					<PageHeader text={"about"} />
+					<PageImage icon={logo} />
+					<PageBody
+						title={"who are we?"}
+						text={
+							"Sonar Studios, commonly known as Sonar, is an American diversified multinational mass media and entertainment conglomerate headquartered at Las Vegas in Nevada, USA."
+						}
+					/>
+					<PageBody
+						title={"why roblox?"}
+						text={
+							"got rlly lucky tbh lol."
+						}
+					/>
+					<PageBody
+						title={"how many toes have we sucked?"}
+						text={
+							"join us and you can find out and add 10 😏"
+						}
+					/>
+				</div>
+			</div>
+
+			<div className="content">
+				<PageHeader text={"titles"} />
+				<div className="grid">
+					<ContentCard
+						img={dragonadventures_title}
+						title={"dragon adventures"}
+						link={"https://www.roblox.com/games/3475397644"}
+						body={
+							"Hatch eggs and raise your dragons to become powerful beasts in a variety of immersive worlds"
+						}
+					/>
+					<ContentCard
+						img={sonaria_title}
+						title={"creatures of sonaria"}
+						link={"https://www.roblox.com/games/5233782396"}
+						body={
+							"Grow, fight and survive with other players as a fictional creature in the beautiful world of Sonaria"
+						}
+					/>
+					<ContentCard
+						img={captive_title}
+						title={"captive"}
+						link={"https://www.roblox.com/games/3388553819"}
+						body={
+							"Hack the computers, escape the beast, unlock the exits, and escape captivity"
+						}
+					/>
+					<ContentCard
+						img={warships_title}
+						title={"warships"}
+						link={"https://www.roblox.com/games/334218733"}
+						body={
+							"Set sail in massive battleships for control over the seas"
+						}
+					/>
+					<ContentCard
+						img={sunnyshores_title}
+						title={"sunny shores"}
+						link={"https://www.roblox.com/games/7071201807"}
+						body={
+							"Build your dream home, dress up, party with friends and more in our tropical island city"
+						}
+					/>
+					<ContentCard
+						img={felinesdestiny_title}
+						title={"feline's destiny"}
+						link={"https://www.roblox.com/games/2586423667"}
+						body={
+							"Explore a rainforest, survive disasters and roleplay with friends as a wild cat"
+						}
+					/>
 				</div>
 			</div>
 
 			{/* page footer */}
-			<div className="footer"></div>
+			<div className="section">
+				<div className="footer">
+					
+				</div>
+			</div>
 		</div>
 	);
 };
