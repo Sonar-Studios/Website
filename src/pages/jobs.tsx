e3b5883d6e2235dccf39eb0ce51eeb895a69fcec
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-08-10 00:01:57 -0700

    add jobs page

---
+++ src/pages/jobs.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "../stylesheets/styles.css";
+
+// we use a functional component for initial render because we can't use
+// hooks in class components, it sucks but that's what we gotta do
+export default () => {
+	useEffect(() => {
+		document.title = "Sonar Jobs";
+	}, []);
+
+	return (
+		<div className="page">
+			<div className="page-wrap">
+				<Header />
+			</div>
+
+			<Footer />
+		</div>
+	)
+}
