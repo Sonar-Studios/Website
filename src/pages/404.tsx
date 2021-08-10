7f6b32858637cb36952e9af6a3f49e58b5b1e156
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-08-10 00:01:21 -0700

    add new 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import PageHeader from "../components/pageHeader";
+import "../stylesheets/styles.css";
+
+// we use a functional component for initial render because we can't use
+// hooks in class components, it sucks but that's what we gotta do
+export default () => {
+	useEffect(() => {
+		document.title = "Sonar Contact";
+	}, []);
+
+	return (
+		<div>
+			<Header />
+
+			<div className="content">
+				<PageHeader text={"404"} />
+				<div>
+					This page doesn't seem to exist
+				</div>
+			</div>
+
+			<Footer />
+		</div>
+	)
+}
