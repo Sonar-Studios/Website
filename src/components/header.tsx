0fdfe9024d86d7c8df23efeeda6c8f770ef54d0e
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2023-04-24 12:22:28 -0700

    updated website + relinked addresses

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,18 +8,32 @@ interface HeaderProps {}
 export default class Header extends Component<HeaderProps, HeaderState> {
 	render() {
 		return (
-			<div className="header">
-				<SonarStudios />
-				<div className="header-buttons">
-					<Link className="header-link link" to="/">
-						home
-					</Link>
-					<Link className="header-link link" to="/jobs">
-						jobs
-					</Link>
-					{/* <Link className="link" to="/contact">
+			<div className="sticky">
+				<div className="ta-banner">
+					<a
+						className="ta-banner-title"
+						href="https://twinatlas.com/"
+						target="_blank"
+						title="Check out Twin Atlas!"
+					>
+						RedManta Games and Sonar Studios have merged to form
+						Twin Atlas!
+					</a>
+				</div>
+				<div className="header">
+					<SonarStudios />
+					<div className="header-buttons">
+						<a className="header-link link" 
+						href="https://twinatlas.com/careers/"
+						target="_blank"
+						title="Check out our opportunities!"
+						>
+							jobs
+						</a>
+						{/* <Link className="link" to="/contact">
 						contact
 					</Link> */}
+					</div>
 				</div>
 			</div>
 		);
